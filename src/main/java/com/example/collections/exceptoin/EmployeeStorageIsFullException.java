437a08e4e9f6781ac0e45eae70cb3098413bc057
DaryaKolosova <darya-kolosova@mail.ru>
2024-12-19 17:21:30 +0300

    Done

---
+++ src/main/java/com/example/collections/exceptoin/EmployeeStorageIsFullException.java
@@ -0,0 +1,12 @@
+package main.java.com.example.collections.exceptoin;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class EmployeeStorageIsFullException extends RuntimeException {
+    public EmployeeStorageIsFullException(String message) {
+        super("Превышен лимит количества сотрудников в фирме");
+    }
+}
---
+++ src/main/java/com/example/collections/service/DepartmentService.java
@@ -0,0 +1,18 @@
+package main.java.com.example.collections.service;
+
+import com.example.collections.model.Employee;
+
+import java.util.List;
+import java.util.Map;
+
+public interface DepartmentService {
+    List<Employee> getEmployeesByDepartmentId(int departmentId);
+
+    int getSalarySumByDepartmentId(int departmentId);
+
+    int getMaxSalaryByDepartmentId(int departmentId);
+
+    int getMinSalaryByDepartmentId(int departmentId);
+
+    Map<Integer, List<Employee>> getAllEmployeesByDepartment();
+}
